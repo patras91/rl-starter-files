ec62c52b859c91a402ca1c2117d61502a874afdf
Sunandita Patra <patras@umd.edu>
2020-11-30 00:41:12 -0500

    Adding the training file

--- utils/env.py
+++ utils/env.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append("../gym-minigrid/")
 
 def make_env(env_key, seed=None):
+    print("env key is" , env_key)
     env = gym.make(env_key)
     env.seed(seed)
     return env
